bbee2294be7a219d9a3294dfdaba3bc9e484e646
Johannes Burgers <45068940+joostburgers@users.noreply.github.com>
2025-04-14 14:31:24 -0400

    all images added

--- config.js
+++ config.js
@@ -21,15 +21,15 @@ var config = {
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'slug-style-id',
-            alignment: 'left',
+            id: 'opening',
+            alignment: 'fully',
             hidden: false,
-            title: 'San Francisco',
-            image: './assets/san-fran.jpeg',
-            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
+            title: 'The Color Purple',
+            image: './assets/color_purple_cover.jpeg',
+            description: 'This is where I insert a description of the novel',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
+                center: [-90.3150946, 38.6605671],
+                zoom: 4,
                 pitch: 60,
                 bearing: 0
             },
@@ -51,15 +51,15 @@ var config = {
             ]
         },
         {
-            id: 'second-identifier',
+            id: 'chapter-one',
             alignment: 'right',
             hidden: false,
-            title: 'Washington, D.C.',
-            image: './assets/washington-dc.jpg',
-            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
+            title: 'Someplace Georgia',
+            image: './assets/alice_walker_childhood_home.jpg',
+            description: 'Description of the second location.',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
+                center: [-83.5573651, -32.5573651], 
+                zoom: 6.5,
                 pitch: 60,
                 bearing: -43.2,
                 // flyTo additional controls-
@@ -76,14 +76,14 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'third-identifier',
+            id: 'chapter-two',
             alignment: 'left',
             hidden: false,
-            title: 'Geneva',
-            image: './assets/geneva.jpg',
-            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
+            title: 'Olinka',
+            image: './assets/firestone_farm.jpg',
+            description: 'Description of the Olinka people',
             location: {
-                center: [6.15116, 46.20595],
+                center: [-10.378889436404226, 6.251275915595865], 
                 zoom: 12.52,
                 pitch: 8.01,
                 bearing: 0.00
@@ -95,20 +95,20 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'fourth-chapter',
+            id: 'chapter-three',
             alignment: 'fully',
             hidden: false,
-            title: 'Buenos Aires',
-            image: './assets/buenos-aires.jpg',
-            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            title: 'End',
+            image: './assets/color_purple_field.jpg',
+            description: 'Close of the novel.',
             location: {
-                center: [-58.54195, -34.71600],
+                center: [-41.47212410646295, 34.781162343274076], 
                 zoom: 4,
-                pitch: 0,
+                pitch: 30,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
