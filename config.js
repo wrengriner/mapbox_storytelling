dc937ce2f1155c789301c9af23c26cc0e44935b2
wrengriner <grinerwa@dukes.jmu.edu>
2025-05-06 15:15:40 -0400

    change

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ var config = {
             image: './assets/alice_walker_childhood_home.jpg',
             description: 'Description of the second location.',
             location: {
-                center: [-89.48848661057376, -89.48848661057376], 
+                center: [-89.48848661057376, 34.28249827482276], 
                 zoom: 6.5,
                 pitch: 60,
                 bearing: -43.2,
