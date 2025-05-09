0737e06e746ee092b0d7e10c05a69e0f3a34831f
wrengriner <grinerwa@dukes.jmu.edu>
2025-05-08 22:53:44 -0400

    change

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
     title: 'Mapping The South: Economy and Position',
-    subtitle: 'The Economic and locality Comparison of Women and Men Across The South',
+    subtitle: 'The Economic and Locality Comparison of Women and Men Across The South',
     byline: 'Wren Griner and Chloe Holloway',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
